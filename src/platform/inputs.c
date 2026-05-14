db2029a665bd055c3627b0a3901c5fd1ea7cbc5d
Hüseyin Emirhan Solak <86725368+huseyinemirhan@users.noreply.github.com>
2026-05-14 18:02:05 +0300

    remove artifacts from repo

---
+++ src/platform/inputs.c
@@ -0,0 +1,5 @@
+//
+// Created by MSI Cyborg on 5.05.2026.
+//
+
+#include "inputs.h"
---
+++ src/platform/inputs.h
@@ -0,0 +1,8 @@
+//
+// Created by MSI Cyborg on 5.05.2026.
+//
+
+#ifndef GB_EMULATOR_CORE_INPUTS_H
+#define GB_EMULATOR_CORE_INPUTS_H
+
+#endif //GB_EMULATOR_CORE_INPUTS_H
